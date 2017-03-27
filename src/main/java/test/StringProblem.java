5031abf8e04b6e954ad3085c770be2c8e7d3e6da
skaja <skaja@D-SJC-10004780.corp.ebay.com>
2017-03-26 19:13:11 -0700

    Changes are committed

--- src/main/java/test/StringProblem.java
+++ src/main/java/test/StringProblem.java
@@ -1,90 +1,78 @@
 package test;
+
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class StringProblem{
-	
-	
-	public String replaceChar(String s,String s1){
-		String newString=null;
-		try{
-		 newString=s.replaceAll(s1, "");}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return newString;
-	}
-	
-	// String replace character one at a time
-	public String replaceCharInLoop(String s, char ch){
-		StringBuilder str= new StringBuilder();
-		String newString=null;
-		try{
-		char[] c=s.toCharArray();
-		
-		for(char i:c){
-			if(i!=ch){
-				str.append(i);
+public class StringProblem {
+
+	// Replace character from string using string utils
+	public String replaceChar(String s, String s1) {
+		String newString = null;
+		if (s == null)
+			return null;
+		else {
+			try {
+				newString = s.replaceAll(s1, "");
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
 			}
-			
-			
+			return newString;
+		}
+	}
+
+	// String replace character one at a time in aloop
+	public String replaceCharInLoop(String s, char ch) {
+		if (s == null)
+			return null;
+		else {
+			StringBuilder str = new StringBuilder();
+			String newString = null;
+			try {
+				char[] c = s.toCharArray();
+
+				for (char i : c) {
+					if (i != ch) {
+						str.append(i);
+					}
+
+				}
+
+				newString = str.toString();
+			}
+
+			catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			}
+			return newString;
 		}
 
-		 newString=str.toString();
-		}
-		
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return newString;
-		
-		
-		
-		
 	}
-	
-	
-	
-	@DataProvider(name="StringData")
-	public Object[][] stringdata1(){
-		
-		Object[][]o ={{"abcdcd","abdd"},
-				{"ABCDCD","ABCDCD"},
-				{null,null}
-				,{"",""}};
+
+	// Test the methods with different set of data
+
+	@DataProvider(name = "StringData")
+	public Object[][] stringdata1() {
+
+		Object[][] o = {{ "abcdcd", "abdd" }, { "ABCDCD", "ABCDCD" },
+				{ null, null }, { "", "" } };
 		return o;
-		
+
 	}
-//	@DataProvider(name="StringData2")
-//	public Object[][] stringdata(){
-//		
-//
-//		Object[][]o ={{"abcdcd","abdd"},
-//				{"ABCDCD","ABCDCD"},
-//				{null,null}
-//				,{"",""}};
-//		return o;
-//		
-//	}
-	
-	
-	@Test(dataProvider="StringData")
-	
-	public void testStringreplce(String s,String s1){
-		
-		String newString=replaceChar(s,"c");
-		
-		Assert.assertEquals(newString,s1);
+
+	@Test(dataProvider = "StringData")
+	public void testStringReplce(String s, String s1) {
+
+		String newString = replaceChar(s, "c");
+
+		Assert.assertEquals(newString, s1);
 	}
-	
-@Test(dataProvider="StringData")
-	
-	public void testStringreplceCharInLoop(String s,String s1){
-		
-		String newString= replaceCharInLoop(s,'c');
-		
-		Assert.assertEquals(newString,s1);
+
+	@Test(dataProvider = "StringData")
+	public void testStringReplceCharInLoop(String s, String s1) {
+
+		String newString = replaceCharInLoop(s, 'c');
+
+		Assert.assertEquals(newString, s1);
 	}
-	
-	
+
 }
